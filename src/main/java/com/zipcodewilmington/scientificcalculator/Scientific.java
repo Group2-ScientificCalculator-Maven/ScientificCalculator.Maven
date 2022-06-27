35799ed56c82be669dd1998feb7e0f9479754a04
Gamze <gamze@zipcodersmbp81.myfiosgateway.com>
2022-06-26 23:10:33 -0400

    Scientific update

--- src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
@@ -1,16 +1,22 @@
 package com.zipcodewilmington.scientificcalculator;
 import java.lang.Math;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import javax.swing.JFrame;
 
 public class Scientific {
-    public static double cos(double numA) {
+    public double cos(double numA) {
         return Math.cos(numA);
     }
 
-    public static double sin(double numB) {
+    public double sin(double numB) {
+
         return Math.sin(numB);
     }
 
-    public static double tan(double numC) {
+    public double tan(double numC) {
+
         return Math.tan(numC);
     }
 
@@ -19,49 +25,37 @@ public class Scientific {
                 return Math.acos(numD);{
             }*/
 
-    public static void acos(String[] args) {
-
-        // get a variable x which is equal to PI/2
-        double x = Math.PI / 2;
-
-        // convert x to radians
-        x = Math.toRadians(x);
-
-        // get the arc cos of x
-        System.out.println("Math.acos(" + x + ")=" + Math.acos(x));
+    public double acos(double x) {
+        return Math.acos(x);
     }
 
-    public static void asin(String[] args) {
 
-        // get a variable x which is equal to PI/2
-        double y = Math.PI / 4;
-
-        // convert x to radians
-        y = Math.toRadians(y);
-
-        // get the arc sine of x
-        System.out.println("Math.asin(" + y + ")=" + Math.asin(y));
+    public double asin( double y) {
+        return Math.asin(y);
     }
 
-    public static void atan(String[] args) {
 
-        // get a variable x which is equal to PI/2
-        double z = Math.PI / 8;
-
-        // convert x to radians
-        z = Math.toRadians(z);
-
-        // get the arc tan of x
-        System.out.println("Math.atan(" + z + ")=" + Math.atan(z));
+    public double atan(double z) {
+        return Math.atan(z);
     }
 
-    public static void main(String args[]) {
-        double a = -2.55;
-        double b = 1.0 / 0;
-        double c = 0, d = 145.256;
 
-        System.out.println(Math.log(a));
-        System.out.println(Math.log(b));
-        System.out.println(Math.log(c));
-    }
+       public static void main(String[] args) {
+    JFrame frame = new JFrame();
+      frame.setSize(800, 600);
+              frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+              GraphicsDevice graphics = GraphicsEnvironment.getLocalGraphicsEnvironment()
+              .getDefaultScreenDevice();
+              graphics.setFullScreenWindow(frame);
+              graphics.setDisplayMode(new DisplayMode(800, 600, 32, 60));
+              frame.setVisible(true);
+              }
 }
+
+
+
+
+
+
+
+
